0f25defcadc3ee5693dfc46f1c3f2650af457669
Blastose <blastoisejay@gmail.com>
2023-10-18 11:57:08 -0700

    Jump to timestamp in when clicking on timestamp link

--- src/lib/components/watch/Watch.svelte
+++ src/lib/components/watch/Watch.svelte
@@ -9,7 +9,36 @@
 	export let commentObject: GetCommentsById | undefined;
 
 	let videoTitleElement: HTMLParagraphElement;
+	let videoElement: HTMLVideoElement;
 	$: stream = video.formatStreams.at(1);
+
+	function handleAnchorClick(e: MouseEvent) {
+		const target = e.target as HTMLElement | null;
+		const a = target?.closest('a');
+
+		if (a?.tagName !== 'A') return;
+		if (!a.href) return;
+
+		const [_, searchParamsString] = a.href.split('?');
+		const searchParams = new URLSearchParams(searchParamsString);
+		const timestamp = searchParams.get('t');
+		if (!timestamp) return;
+		if (searchParams.get('v') !== video.videoId) return;
+
+		e.preventDefault();
+		jumpToTimestamp(Number(timestamp));
+		window.scrollTo({ top: 0, behavior: 'smooth' });
+	}
+
+	function setupHandleAnchorClick(el: HTMLDivElement) {
+		el.addEventListener('click', handleAnchorClick);
+
+		return {
+			destroy: () => {
+				el.removeEventListener('click', handleAnchorClick);
+			}
+		};
+	}
 
 	function handleDescriptionCollapse() {
 		const rect = videoTitleElement.getBoundingClientRect();
@@ -17,13 +46,21 @@
 			videoTitleElement.scrollIntoView({ behavior: 'smooth' });
 		}
 	}
+
+	function jumpToTimestamp(timestamp: number) {
+		videoElement.currentTime = timestamp;
+		videoElement.play();
+	}
 </script>
 
-<div class="grid grid-cols-1 lg:grid-cols-[1fr_256px] xl:grid-cols-[1fr_400px] gap-2 sm:gap-8">
+<div
+	use:setupHandleAnchorClick
+	class="grid grid-cols-1 lg:grid-cols-[1fr_256px] xl:grid-cols-[1fr_400px] gap-2 sm:gap-8"
+>
 	<div class="flex flex-col gap-4">
 		{#if stream}
 			<div class="rounded-lg overflow-hidden">
-				<video class="w-full" src={stream.url} controls muted />
+				<video bind:this={videoElement} class="w-full" src={stream.url} controls muted />
 			</div>
 		{/if}
 
