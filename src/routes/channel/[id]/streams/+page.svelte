07261ef9eb87839c2f7b6e121590e3796ee6a62a
Blastose <blastoisejay@gmail.com>
2023-10-21 09:13:50 -0700

    Add text if there are no streams

--- src/routes/channel/[id]/streams/+page.svelte
+++ src/routes/channel/[id]/streams/+page.svelte
@@ -13,39 +13,45 @@
 
 <svelte:head><title>{buildPageTitle(data.channel.author)}</title></svelte:head>
 
-<div class="flex flex-col gap-4">
-	<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4">
-		{#each data.streams.videos as video}
-			{@const thumbnail =
-				video.videoThumbnails.find((v) => v.quality === 'medium') ?? video.videoThumbnails[0]}
-			<VideoCard
-				author={video.author}
-				authorUrl={video.authorUrl}
-				authorVerified={video.authorVerified}
-				lengthSeconds={video.lengthSeconds}
-				publishedText={video.publishedText}
-				title={video.title}
-				videoId={video.videoId}
-				videoThumbnail={thumbnail}
-				viewCountText={video.viewCountText}
-				type="stream"
-			/>
-		{/each}
+{#if data.streams.videos.length > 0}
+	<div class="flex flex-col gap-4">
+		<div
+			class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4"
+		>
+			{#each data.streams.videos as video}
+				{@const thumbnail =
+					video.videoThumbnails.find((v) => v.quality === 'medium') ?? video.videoThumbnails[0]}
+				<VideoCard
+					author={video.author}
+					authorUrl={video.authorUrl}
+					authorVerified={video.authorVerified}
+					lengthSeconds={video.lengthSeconds}
+					publishedText={video.publishedText}
+					title={video.title}
+					videoId={video.videoId}
+					videoThumbnail={thumbnail}
+					viewCountText={video.viewCountText}
+					type="stream"
+				/>
+			{/each}
+		</div>
+		<InfiniteScrolling
+			bind:results={data.streams.videos}
+			bind:noMoreSearchResults
+			fetchMoreResults={async () => {
+				const invidious = new Invidious('https://invidious.fdn.fr');
+				const res = await invidious.getChannelStreams(data.channel.authorId, {
+					sort_by: 'newest',
+					continuation
+				});
+				console.log(res);
+				data.streams.continuation = res.continuation;
+				noMoreSearchResults = !Boolean(data.streams.continuation);
+				return res.videos;
+			}}
+			onSuccess={async () => {}}
+		/>
 	</div>
-	<InfiniteScrolling
-		bind:results={data.streams.videos}
-		bind:noMoreSearchResults
-		fetchMoreResults={async () => {
-			const invidious = new Invidious('https://invidious.fdn.fr');
-			const res = await invidious.getChannelStreams(data.channel.authorId, {
-				sort_by: 'newest',
-				continuation
-			});
-			console.log(res);
-			data.streams.continuation = res.continuation;
-			noMoreSearchResults = !Boolean(data.streams.continuation);
-			return res.videos;
-		}}
-		onSuccess={async () => {}}
-	/>
-</div>
+{:else}
+	<p class="w-full text-center">This channel has no videos.</p>
+{/if}
