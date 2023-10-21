cdb1b65484d838d36bc17ac80a8052ba34b5c381
Blastose <blastoisejay@gmail.com>
2023-10-20 18:58:49 -0700

    Refactor infinite scrolling to its own component

--- src/lib/components/search/Results.svelte
+++ src/lib/components/search/Results.svelte
@@ -1,47 +1,17 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import type { GetSearch } from '$lib/invidious/types';
-	import { onMount } from 'svelte';
-	import Loading from '../layout/Loading.svelte';
 	import ChannelSearchCard from './ChannelSearchCard.svelte';
 	import PlaylistSearchCard from './PlaylistSearchCard.svelte';
 	import VideoSearchCard from './VideoSearchCard.svelte';
 	import { getSearchFromUrl } from './utils';
 	import Filter from './filter/Filter.svelte';
+	import InfiniteScrolling from '../layout/InfiniteScrolling.svelte';
 
 	export let results: GetSearch[];
 
 	let pageParam = 2;
-	let loadingElement: HTMLDivElement;
 	let noMoreSearchResults = false;
-	let loadingSearchResults = false;
-
-	onMount(() => {
-		const interactionObserver = new IntersectionObserver(async (entries) => {
-			if (entries[0].intersectionRatio <= 0) return;
-
-			if (loadingSearchResults) return;
-
-			loadingSearchResults = true;
-			try {
-				const searchResults = await getSearchFromUrl($page.url, pageParam);
-				if (searchResults.length === 0) {
-					noMoreSearchResults = true;
-					return;
-				}
-				results = results.concat(searchResults);
-				pageParam++;
-				console.log(searchResults);
-			} catch {}
-			loadingSearchResults = false;
-		});
-
-		interactionObserver.observe(loadingElement);
-
-		return () => {
-			interactionObserver.unobserve(loadingElement);
-		};
-	});
 </script>
 
 <div class="flex flex-col gap-4">
@@ -59,9 +29,17 @@
 		{/if}
 	{/each}
 
-	<div class:hidden={noMoreSearchResults} bind:this={loadingElement}>
-		<Loading />
-	</div>
+	<InfiniteScrolling
+		bind:results
+		bind:noMoreSearchResults
+		fetchMoreResults={async () => {
+			return await getSearchFromUrl($page.url, pageParam);
+		}}
+		onSuccess={async () => {
+			pageParam++;
+		}}
+	/>
+
 	{#if noMoreSearchResults}
 		<p class="text-center">No more results</p>
 	{/if}
